4cca5becf193132984b91e4bb4b1ee717f5c0e4c
masak <masak@af0934a6-d575-4a24-95ca-54447cc808c2>
2007-08-27 16:11:16 +0000

    simplified tests
    git-svn-id: svn+ssh://gatekeeper.ua.sandberg.pp.se/home/masak/work-svn-repo/work/hobbies/iupac-alkanes@123 af0934a6-d575-4a24-95ca-54447cc808c2

--- BranchedAlkanesTest.java
+++ BranchedAlkanesTest.java
@@ -1,8 +1,26 @@
 class BranchedAlkanesTest extends AlkanesTest {
 
-      private BranchedAlkanesTest( String name, int testsPlanned ) {
-         super( name, testsPlanned );
-      }
+    private BranchedAlkanesTest( String name, int testsPlanned ) {
+      super( name, testsPlanned );
+    }
+
+    private String inMiddleOf(int halfLength, String alkyl) {
+      StringBuilder sb = new StringBuilder();
+
+      for ( int i = 0; i < halfLength + 1; i++ )
+        sb.append("C(");
+
+      sb.append(alkyl.substring(1));
+
+      for ( int i = 0; i < halfLength - 1; i++ )
+        sb.append("C(");
+      sb.append("C");
+
+      for ( int i = 0; i < halfLength; i++ )
+        sb.append("))");
+
+      return sb.toString();
+    }
 
       public void runTests() {
          is( new Alkane("C(C(C(CC(C))))").iupacName(),
@@ -21,28 +39,63 @@ class BranchedAlkanesTest extends AlkanesTest {
              "5-Methyl-4-propylnonane",
              "Direction chosen so as to give lowest possible numbers III" );
 
-         is( new Alkane("C(C(C(C(C(C(C(C(CC(C(C(C))))"
+         is( new Alkane("C(C(C(C(C(C(C("
+                        + "C(CC(C(C(C))))"
                         + "C(C(C(C(C(C))))))))))))").iupacName(),
              "7-(1-Methylpentyl)tridecane",
              "Branches are numbered from the trunk out along longest chain I"
            );
          
-         is( new Alkane("C(C(C(C(C(C(C(C(C(CC(C(C))))"
+         is( new Alkane("C(C(C(C(C(C(C("
+                        + "C(C(CC(C(C))))"
                         + "C(C(C(C(C(C))))))))))))").iupacName(),
              "7-(2-Methylpentyl)tridecane",
              "Branches are numbered from the trunk out along longest chain II"
            );
          
-         is( new Alkane("C(C(C(C(C(C(C(C(C(C(C(C(C(CC)))))"
+         is( new Alkane("C(C(C(C(C(C(C("
+                        + "C(C(C(C(C(C(CC)))))"
                         + "C(C(C(C(C(C(C))))))))))))))").iupacName(),
              "8-(5-Methylhexyl)pentadecane",
              "Branches are numbered from the trunk out along longest chain III"
            );
+
+         is( new Alkane( inMiddleOf(4, "-C(CC)") ).iupacName(),
+             "5-Isopropylnonane",
+             "Isopropyl" );
+         
+         is( new Alkane( inMiddleOf(4, "-C(C(CC))") ).iupacName(),
+             "5-Isobutylnonane",
+             "Isobutyl" );
+         
+         is( new Alkane( inMiddleOf(4, "-C(CC(C))") ).iupacName(),
+             "5-sec-butylnonane",
+             "sec-butyl" );
+         
+         is( new Alkane( inMiddleOf(5, "-C(CCC)") ).iupacName(),
+             "6-tert-butylundecane",
+             "tert-butyl" );
+         
+         is( new Alkane( inMiddleOf(6, "-C(C(C(CC)))") ).iupacName(),
+             "7-Isopentyltridecane",
+             "Isopentyl" );
+         
+         is( new Alkane( inMiddleOf(5, "-C(C(CCC))") ).iupacName(),
+             "6-Neopentylundecane",
+             "Neopentyl" );
+         
+         is( new Alkane( inMiddleOf(5, "-C(CCC(C))") ).iupacName(),
+             "6-tert-Pentylundecane",
+             "tert-Pentyl" );
+         
+         is( new Alkane( inMiddleOf(6, "-C(C(C(C(CC))))") ).iupacName(),
+             "7-Isohexyltridecane",
+             "Isohexyl" );
       }
 
       public static void main( String args[] ) {
          new BranchedAlkanesTest( "Branched alkanes",
-                                  7
+                                  15
             ).test();
       }
 }
