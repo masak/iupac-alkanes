c5dc4ae23edab5668956b1c067f30f55bf557c70
masak <masak@af0934a6-d575-4a24-95ca-54447cc808c2>
2007-08-31 08:27:52 +0000

    improved alkane construction, moved utility methods into AlkanesTest
    git-svn-id: svn+ssh://gatekeeper.ua.sandberg.pp.se/home/masak/work-svn-repo/work/hobbies/iupac-alkanes@139 af0934a6-d575-4a24-95ca-54447cc808c2

--- AlkanesTest.java
+++ AlkanesTest.java
@@ -15,4 +15,81 @@ abstract class AlkanesTest extends Test {
             System.out.println("       Actual: '" + actual   + "'");
          }
       }
+
+    protected String straight(int length) {
+      StringBuilder sb = new StringBuilder();
+
+      for ( int i = 0; i < length - 1; i++ )
+        sb.append("C(");
+
+      sb.append("C");
+      
+      for ( int i = 0; i < length - 1; i++ )
+        sb.append(")");
+
+      return sb.toString();
+    }
+
+    protected String methane() { return straight(1);  }
+    protected String  ethane() { return straight(2);  }
+    protected String propane() { return straight(3);  }
+    protected String  butane() { return straight(4);  }
+    protected String pentane() { return straight(5);  }
+    protected String  hexane() { return straight(6);  }
+    protected String heptane() { return straight(7);  }
+    protected String  octane() { return straight(8);  }
+    protected String  nonane() { return straight(9);  }
+    protected String  decane() { return straight(10); }
+
+    private char afterParentheses(String chain, int startLoc) {
+      int runningLoc = startLoc + 1;
+
+      for ( int level = 1;
+            level > 0 && runningLoc < chain.length();
+            runningLoc++ )
+
+        if (chain.charAt(runningLoc) == '(')
+          level++;
+        else if (chain.charAt(runningLoc) == ')')
+          level--;
+
+      if (runningLoc > chain.length() - 1)
+        throw new IllegalStateException( chain.substring(0,startLoc)
+                                         + "X"
+                                         + chain.substring(startLoc) );
+
+      return chain.charAt(runningLoc);
+    }
+
+    protected String graftOnto(String bigChain, int pos, String smallChain) {
+      for (int i = 0, level = 0; i < bigChain.length(); i++ ) {
+
+        if ( bigChain.charAt(i) == '(' )
+          level++;
+
+        else if ( bigChain.charAt(i) == ')' )
+          level--;
+
+        else if ( level == pos && bigChain.charAt(i) == 'C'
+                  && ((bigChain.charAt(i+1) == '('
+                       && afterParentheses(bigChain, i+1) != 'C')
+                      || bigChain.charAt(i+1) == ')') )
+
+          return bigChain.substring(0, i)
+                 + smallChain
+                 + bigChain.substring(i);
+
+        else if ( level == pos - 1
+                  && bigChain.charAt(i) == 'C' && bigChain.charAt(i+1) == ')' )
+
+          return bigChain.substring(0, i+1)
+                 + smallChain
+                 + bigChain.substring(i+1);
+      }
+
+      throw new IllegalArgumentException(
+        "Could not graft " + smallChain + " onto position " + pos
+        + " of " + bigChain + "."
+      );
+    }
 }
--- BranchedAlkanesTest.java
+++ BranchedAlkanesTest.java
@@ -4,48 +4,6 @@ class BranchedAlkanesTest extends AlkanesTest {
       super( name, testsPlanned );
     }
 
-    private String inMiddleOf(int halfLength, String alkyl) {
-      StringBuilder sb = new StringBuilder();
-
-      for ( int i = 0; i < halfLength + 1; i++ )
-        sb.append("C(");
-
-      sb.append( alkyl.substring(1) );
-
-      for ( int i = 0; i < halfLength - 1; i++ )
-        sb.append("C(");
-      sb.append("C");
-
-      for ( int i = 0; i < halfLength; i++ )
-        sb.append("))");
-
-      return sb.toString();
-    }
-
-    private String straight(int length) {
-      StringBuilder sb = new StringBuilder();
-
-      for ( int i = 0; i < length - 1; i++ )
-        sb.append("C(");
-
-      sb.append("C");
-      
-      for ( int i = 0; i < length - 1; i++ )
-        sb.append(")");
-
-      return sb.toString();
-    }
-
-    private String methane() {
-      return straight(1);
-    }
-
-    private String graft(String bigChain, int pos, String smallChain) {
-      return bigChain.substring(0, 2 * pos)
-             + smallChain
-             + bigChain.substring(2 * pos);
-    }
-
     protected void runTests() {
       // These tests are heavily based on
       // http://www.acdlabs.com/iupac/nomenclature/79/r79_36.htm
@@ -66,56 +24,55 @@ class BranchedAlkanesTest extends AlkanesTest {
           "5-Methyl-4-propylnonane",
           "Direction chosen so as to give lowest possible numbers III" );
 
-      is( new Alkane("C(C(C(C(C(C(C("
-                     + "C(CC(C(C(C))))"
-                     + "C(C(C(C(C(C))))))))))))").iupacName(),
+      String oneMethylPentyl = graftOnto( pentane(), 1, methane() );
+      is( new Alkane( graftOnto(straight(13), 7, oneMethylPentyl) ).iupacName(),
           "7-(1-Methylpentyl)tridecane",
           "Branches are numbered from the trunk out along longest chain I"
         );
          
-      is( new Alkane("C(C(C(C(C(C(C("
-                     + "C(C(CC(C(C))))"
-                     + "C(C(C(C(C(C))))))))))))").iupacName(),
+      String twoMethylPentyl = graftOnto( pentane(), 2, methane() );
+      is( new Alkane( graftOnto(straight(13), 7, twoMethylPentyl)).iupacName(),
           "7-(2-Methylpentyl)tridecane",
           "Branches are numbered from the trunk out along longest chain II"
         );
          
-      is( new Alkane("C(C(C(C(C(C(C("
-                     + "C(C(C(C(C(C(CC)))))"
-                     + "C(C(C(C(C(C(C))))))))))))))").iupacName(),
+      String fiveMethylHexyl = graftOnto( hexane(), 5, methane() );
+      is( new Alkane( graftOnto(straight(15), 8, fiveMethylHexyl) ).iupacName(),
           "8-(5-Methylhexyl)pentadecane",
           "Branches are numbered from the trunk out along longest chain III"
         );
 
-      is( new Alkane( inMiddleOf(4, "-C(CC)") ).iupacName(),
+      is( new Alkane( graftOnto(nonane(), 5, "C(CC)") ).iupacName(),
           "5-Isopropylnonane",
           "Isopropyl" );
          
-      is( new Alkane( inMiddleOf(4, "-C(C(CC))") ).iupacName(),
+      is( new Alkane( graftOnto(nonane(), 5, "C(C(CC))") ).iupacName(),
           "5-Isobutylnonane",
           "Isobutyl" );
          
-      is( new Alkane( inMiddleOf(4, "-C(CC(C))") ).iupacName(),
+      is( new Alkane( graftOnto(nonane(), 5, "C(CC(C))") ).iupacName(),
           "5-sec-Butylnonane",
           "sec-butyl" );
          
-      is( new Alkane( inMiddleOf(5, "-C(CCC)") ).iupacName(),
+      is( new Alkane( graftOnto(straight(11), 6, "C(CCC)") ).iupacName(),
           "6-tert-Butylundecane",
           "tert-butyl" );
          
-      is( new Alkane( inMiddleOf(6, "-C(C(C(CC)))") ).iupacName(),
+      is( new Alkane( graftOnto(straight(13), 7, "C(C(C(CC)))") ).iupacName(),
           "7-Isopentyltridecane",
           "Isopentyl" );
          
-      is( new Alkane( inMiddleOf(5, "-C(C(CCC))") ).iupacName(),
+      is( new Alkane( graftOnto(straight(11), 6, "C(C(CCC))") ).iupacName(),
           "6-Neopentylundecane",
           "Neopentyl" );
          
-      is( new Alkane( inMiddleOf(5, "-C(CCC(C))") ).iupacName(),
+      is( new Alkane( graftOnto(straight(11), 6, "C(CCC(C))") ).iupacName(),
           "6-tert-Pentylundecane",
           "tert-Pentyl" );
          
-      is( new Alkane( inMiddleOf(6, "-C(C(C(C(CC))))") ).iupacName(),
+      is( new Alkane( graftOnto(straight(13),
+                                7,
+                                "C(C(C(C(CC))))") ).iupacName(),
           "7-Isohexyltridecane",
           "Isohexyl" );
 
@@ -123,30 +80,32 @@ class BranchedAlkanesTest extends AlkanesTest {
           "4-Ethyl-3,3-dimethylheptane",
           "Simple radicals are alphabetized before prefixes are inserted" );
 
-      is( new Alkane( "C(C(C(C(C(C(C)C(C("
-                      + "C(CC(CC(C(C))))"
-                      + "C(C(C(C(C(C))))))))))))" ).iupacName(),
+      String oneTwoDiMethylPentyl
+        = graftOnto(graftOnto(pentane(), 1, methane() ), 2, methane());
+      is( new Alkane( graftOnto(
+                        graftOnto(straight(13), 7, oneTwoDiMethylPentyl),
+                        5, ethane()) ).iupacName(),
           "7-(1,2-Dimethylpentyl)-5-ethyltridecane",
           "Complex radicals are alphabetized by their complete names" );
 
-      is( new Alkane( "C(C(C(C(C(C("
-                      + "C(CC(C(C)))"
-                      + "C(C("
-                      + "C(C(CC(C)))"
-                      + "C(C(C(C(C))))))))))))" ).iupacName(),
+      String oneMethylButyl = graftOnto(butane(), 1, methane()),
+             twoMethylButyl = graftOnto(butane(), 2, methane());
+      is( new Alkane( graftOnto(graftOnto(straight(13), 8, twoMethylButyl),
+                                6, oneMethylButyl) ).iupacName(),
           "6-(1-Methylbutyl)-8-(2-methylbutyl)tridecane",
           "Complex radicals with same name give priority to lowest locant" );
 
-      is( new Alkane( graft(graft(straight(8),
-                                  5, methane()),
-                                  4, straight(2)) ).iupacName(),
+      is( new Alkane( graftOnto(graftOnto(straight(8),
+                                          5, methane()),
+                                          4, straight(2)) ).iupacName(),
           "4-Ethyl-5-methyloctane",
           "Side chains on equivalent positions are given numbers in order I"
         );
       
-      is( new Alkane( graft(graft(straight(8),
-                                  5, straight(3)),
-                                  4, graft(straight(2), 1, methane()))
+      is( new Alkane( graftOnto(graftOnto(straight(8),
+                                          5, straight(3)),
+                                          4, graftOnto(
+                                            straight(2), 1, methane()))
                     ).iupacName(),
           "4-Isopropyl-5-propyloctane",
           "Side chains on equivalent positions are given numbers in order II"
